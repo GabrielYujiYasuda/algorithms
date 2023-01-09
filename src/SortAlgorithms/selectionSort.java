7fbfaa4da03e7978e570449cbb3046059fb4dc40
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2023-01-09 17:29:37 -0300

    first commit

---
+++ src/SortAlgorithms/selectionSort.java
@@ -0,0 +1,34 @@
+package SortAlgorithms;
+
+public class selectionSort {
+    public static void main(String[] args) {
+
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        for (int lastIndex = intArray.length - 1; lastIndex > 0; lastIndex--) {
+            int largest = 0;
+
+            for (int i = 1; i <= lastIndex ; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+            }
+            swap(intArray, largest, lastIndex);
+        }
+
+        //Printer
+        for (int j : intArray) {
+            System.out.println(j);
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if(i == j) {
+            return;
+        }
+
+        int temporary = array[i];
+        array[i] = array[j];
+        array[j] = temporary;
+    }
+}
