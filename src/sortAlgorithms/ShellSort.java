04cb5e1aec19de0ede048c3a965d39b11c4baec3
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2023-01-16 14:58:52 -0300

    commit

---
+++ src/sortAlgorithms/ShellSort.java
@@ -0,0 +1,16 @@
+package sortAlgorithms;
+
+public class ShellSort {
+    public static void main(String[] args) {
+
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+
+
+
+
+        //Print array
+        for (int print: intArray)
+            System.out.println(print);
+    }
+}
